61b24ce61ddbe6014fa35b3daa498c4be27e1554
Zael24 <matuszek.rafal99@gmail.com>
2023-08-02 20:36:58 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+const d = new Date();
+let dayOfWeek = d.getDay();
+
+//dayz = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+function getDay(dayz) {
+    return dayz === '0' || dayz === 6 ? 'weekend' : 'weekday';
+}
+
+function getActionType(action) {
+    return action === 'weekend' ? 'have fun' : 'work hard';
+}
+
+app.get('/', (req, res) => {
+    res.render('index.ejs', {
+        day: getDay(dayOfWeek), act: getActionType(getDay())
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
+  
